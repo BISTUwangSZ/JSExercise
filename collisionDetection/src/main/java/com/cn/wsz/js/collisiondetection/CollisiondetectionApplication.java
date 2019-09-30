0531f7517d0b87a482fe7e15ff8c024bdbee602a
wangsizhuo <wangsizhuo@btte.com>
2019-09-30 12:00:48 +0800

    Initial commit

---
+++ collisionDetection/src/main/java/com/cn/wsz/js/collisiondetection/CollisiondetectionApplication.java
@@ -0,0 +1,13 @@
+package com.cn.wsz.js.collisiondetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollisiondetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollisiondetectionApplication.class, args);
+	}
+
+}
---
+++ collisionDetection/src/test/java/com/cn/wsz/js/collisiondetection/CollisiondetectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cn.wsz.js.collisiondetection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollisiondetectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
